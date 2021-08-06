8cec6651691774ec45533e52fb71a515719504d9
Maurício Sousa <morricedev@gmail.com>
2021-08-06 10:05:14 -0300

    Update .env.example

--- config/database.js
+++ config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "cluster0.slbul.mongodb.net"),
+        host: env("DATABASE_HOST"),
         srv: env.bool("DATABASE_SRV", true),
-        port: env.int("DATABASE_PORT", 27017),
+        port: env.int("DATABASE_PORT"),
         database: env("DATABASE_NAME"),
         username: env("DATABASE_USERNAME"),
         password: env("DATABASE_PASSWORD"),
